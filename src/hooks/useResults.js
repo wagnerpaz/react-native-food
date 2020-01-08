f0153294978dda8489f6ce3d15e7865ed237cf0a
Wagner Paz <wagner.o.paz@gmail.com>
2020-01-07 23:27:33 -0300

    Refactored SearchScreen to extract the results' hook logic to a separate file to form a custom hook.

---
+++ src/hooks/useResults.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+    const [results, setResults] = useState([]);
+    const [errorMsg, setErrorMsg] = useState('');
+
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
+
+    const searchApi = async (term = searchTerm) => {
+        try {
+            await setErrorMsg('');
+            const response = await yelp.get('/businesses/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san jose',
+                }
+            });
+            setResults(response.data.businesses);
+        }
+        catch(err) {
+            console.log(err);
+            setErrorMsg('Something went wrong! Try again later.')
+        }
+    };
+
+    return [searchApi, results, errorMsg];
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,34 +2,11 @@ import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import SearchInput from '../components/SearchInput';
-import yelp from '../api/yelp';
-
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMsg, setErrorMsg] = useState('');
-
-    useEffect(() => {
-        searchApi('pasta');
-    }, []);
-
-    const searchApi = async (term = searchTerm) => {
-        try {
-            await setErrorMsg('');
-            const response = await yelp.get('/businesses/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose',
-                }
-            });
-            setResults(response.data.businesses);
-        }
-        catch(err) {
-            setErrorMsg('Something went wrong! Try again later.')
-        }
-    };
+    const [searchApi, results, errorMsg] = useResults();
 
     return (
         <View style={styles.container}>
