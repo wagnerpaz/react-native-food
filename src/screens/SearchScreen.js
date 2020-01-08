0b5896359fd76a0d4c3d735a292b9ca2dac8c1bd
Wagner Paz <wagner.o.paz@gmail.com>
2020-01-07 23:58:58 -0300

    Implemented a Results List component. Divided the results in three areas that shows a specific price range.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import SearchInput from '../components/SearchInput';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchApi, results, errorMsg] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        return results.filter( result => {
+            return result.price === price;
+        });
+    };
 
     return (
         <View style={styles.container}>
@@ -17,6 +24,18 @@ const SearchScreen = () => {
             />
             {errorMsg ? <Text>{errorMsg}</Text> : null}
             <Text>We have found {results.length} results.</Text>
+            <ResultsList
+                title="Cost Effective"
+                results={filterResultsByPrice('$')}
+            />
+            <ResultsList
+                title="Bit Pricier"
+                results={filterResultsByPrice('$$')}
+            />
+            <ResultsList
+                title="Big Spender"
+                results={filterResultsByPrice('$$$')}
+            />
         </View>
     );
 };
